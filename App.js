ffae03dde0558bac70f387237e8f37fe8e12fbce
rdshield <rdshield@uci.edu>
2017-10-22 17:06:13 -0700

    First Commit pt 3 - RDS adding Logo

--- App.js
+++ App.js
@@ -5,12 +5,13 @@ import MapView from 'react-native-maps';
 
 export default class mapsTest extends Component {
   render() {
-    // let logo = { uri: 'https://lh3.googleusercontent.com/uGvFtrf37kN0UqmQM5M2nQ5PT78UCzNcUoudilWZJPgtE3PJ4hS9ZvkYQXZS73td46pB7FUlozyFtVd0BAxJ8_2pqBepF4Ti9aQwPwk8EV48jNh63dTmjEhyz0KnBZcMhxDRJdMkW2i1b7_H5ag3QsXmB_Tf-4OENW7LD9QC4ejC3vsKtT3gMXPyrn2AoKyDDQQ3ZGN5eB-bKadtnXySBHkYaexU5THglrSgUoNa-NyacghISbO_x5ybvTGQsJ0tAlAJDwn2MI_Pa9pwQtydKAIsAtSrdxrC-otyT3mgsPMNTH8NRPwcXaQSp5-up647DkF3SKGuZGWx_eIa6GVmMa2zxa4toGZG5euYCrzYLHup6X_zaNWXr-u3A-OEj84yoR1quwX6ypszWL0xIqTw5pffk7BrwV_Q4sRGrys3CHRUeDBalBfSpm-ah8VXP4q2SQCipdIS65lABgvLETDpD3IVI5Nvnd3KxmSs26Ir5YLqWwv1kC-OPr3pivkNWFbpeP58V17xE23iHCbsmaznGtQOxOBOWdinRG0Ry0fgtCWYcRI-31dLpF2-U9yQQAgyfCC8uejhQndgmWG9Kv_UMIN3b9_3YK16G7Ugp2bWzlUCdDtvTHW-aywp4ni_-Sh6Y9Bc9Qh7rbwdE7_oVdq9HSB337kZHpa1y24=w576-h262-no'};
-
+    let logo = { uri: 'https://lh3.googleusercontent.com/UKHz9AkBFV-a5c1TTRgvG8jk5YVjpOQX77vRYoFhT_ZAaZk487uvHznvSHWP4okpM6FOKNML_Vqj_WH7DwWZ37RxNjMMTG8gIcZryP1y_uygyLfMS34VUaqECq-3Y6WmE084fvJSjjczI5LIEMZULUmrYoIuuT9gOXOkNpJkGgfIL-t5OcL4goYz-HFsJgmmLFFJBVJKGC50JolEz5Wi5iuOM83BqCihVvXH4xjt0DQt_yl9vad28DvKrfoSYBfn9S7jALtPzGmYq_w-DH7p3TNEFYSYRILWa6veUju4QFMge5fx02v-fUilsxShYYP3YVRPeWGAqF-lJaeOKRjMUUfPG1Sn4HvcuSlXEHKNgzddOflemMW0goa9lWBfaChw1xje7ltEgL3DsX09ZryBZIAEpCR5oj8O9QOfMqkQVOv3PPD9Y9iSCGIrz1jb9RnSvggU9Tw_29_7TdVQYqUZPahLkT1IDq3CXhN79wlr3RMBuH3P3hzsZX7HBGn44FGmsImLNvuy1Q7tSS3Y6g_0QjC6-Ia-JSfwl3iyucCuJJ0bX4BW8K1lxuaTqcr7sWX7UGTdrKwTjGF0TQMzq7K31xbn1nKQunNda50rtxtuUfVlKThi047SVqB7kAQuKYsY-lDdcChEgCiNLAOYyGiXI9IW8KUchV8F1io=w524-h222-no'}
     return (
       <View style={{flex:1}}>
         <View style={{height: 25, backgroundColor: 'black'}}/>
-        <View style={{height: 40, backgroundColor: 'gold'}}/>
+        <View style={{alignItems:'center',height: 50, backgroundColor: 'gold'}}>
+          <Image source={logo} style={{width: 125, height: 50}}/>
+        </View>
         <View style={{flex:1, backgroundColor: 'steelblue'}}>
           <MapView
             style={styles.map}
