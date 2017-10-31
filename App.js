a925967c3d36f254b053daf96f1ea764d8aab4da
rdshield <rdshield@uci.edu>
2017-10-31 11:29:39 -0700

    Added comments and changed loading screen layout

--- App.js
+++ App.js
@@ -1,23 +1,39 @@
-import React, { Component } from 'react';
-import { Button, Dimensions, Image, StyleSheet, Text, View } from 'react-native';
-import { StackNavigator } from 'react-navigation';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-import Polyline from '@mapbox/polyline';
+import React, { Component } from 'react';   //Needed for Creation of React-Native Objects
+import { Button, Dimensions, Image, StyleSheet, Text, View } from 'react-native'; //Objects used in app
+import { StackNavigator } from 'react-navigation'; //Navigation Libraries
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'; //Map Generation Libraries
+import Polyline from '@mapbox/polyline'; //Used for Map navigation overlay (not yet initiated)
 
-// let logo = { uri: 'https://lh3.googleusercontent.com/UKHz9AkBFV-a5c1TTRgvG8jk5YVjpOQX77vRYoFhT_ZAaZk487uvHznvSHWP4okpM6FOKNML_Vqj_WH7DwWZ37RxNjMMTG8gIcZryP1y_uygyLfMS34VUaqECq-3Y6WmE084fvJSjjczI5LIEMZULUmrYoIuuT9gOXOkNpJkGgfIL-t5OcL4goYz-HFsJgmmLFFJBVJKGC50JolEz5Wi5iuOM83BqCihVvXH4xjt0DQt_yl9vad28DvKrfoSYBfn9S7jALtPzGmYq_w-DH7p3TNEFYSYRILWa6veUju4QFMge5fx02v-fUilsxShYYP3YVRPeWGAqF-lJaeOKRjMUUfPG1Sn4HvcuSlXEHKNgzddOflemMW0goa9lWBfaChw1xje7ltEgL3DsX09ZryBZIAEpCR5oj8O9QOfMqkQVOv3PPD9Y9iSCGIrz1jb9RnSvggU9Tw_29_7TdVQYqUZPahLkT1IDq3CXhN79wlr3RMBuH3P3hzsZX7HBGn44FGmsImLNvuy1Q7tSS3Y6g_0QjC6-Ia-JSfwl3iyucCuJJ0bX4BW8K1lxuaTqcr7sWX7UGTdrKwTjGF0TQMzq7K31xbn1nKQunNda50rtxtuUfVlKThi047SVqB7kAQuKYsY-lDdcChEgCiNLAOYyGiXI9IW8KUchV8F1io=w524-h222-no'
-// }
+/*
+  !!!Note: PLEASE SEE LOADSCREEN CLASS FOR CLASS-BASED COMMENTS (SIMILAR FOR ALL SCREEN CLASSES)!!!
+*/
 
+//Loading Screen Class - Used for Screen #1 (Loading Screen), including navigation settings and view
 class LoadScreen extends React.Component {
+  //Used for Screen-to-screen navigation and the design of the top-bar
   static navigationOptions = {
-      title: 'Loading',
-      header: null,
+      title: 'Loading',                 //Reference Name of Screen
+      header: null,                     //null means that the top-bar is not visible
   };
 
+  //How this screen will be rendered
   render() {
+    //Required for navigation (needed in each screen class)
     const { navigate } = this.props.navigation;
+
+    //How the page is rendered for viewing
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button title= " Go to Home " onPress={() => navigate('Home') } />
+      //Viewing Container for files (if there is more than one object, there must be a enclosing container)
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor:'goldenrod' }}>
+      //Add image to the screen
+      <Image
+            style={{width: 200, height: 75}}
+            source={{uri: 'https://lh3.googleusercontent.com/UKHz9AkBFV-a5c1TTRgvG8jk5YVjpOQX77vRYoFhT_ZAaZk487uvHznvSHWP4okpM6FOKNML_Vqj_WH7DwWZ37RxNjMMTG8gIcZryP1y_uygyLfMS34VUaqECq-3Y6WmE084fvJSjjczI5LIEMZULUmrYoIuuT9gOXOkNpJkGgfIL-t5OcL4goYz-HFsJgmmLFFJBVJKGC50JolEz5Wi5iuOM83BqCihVvXH4xjt0DQt_yl9vad28DvKrfoSYBfn9S7jALtPzGmYq_w-DH7p3TNEFYSYRILWa6veUju4QFMge5fx02v-fUilsxShYYP3YVRPeWGAqF-lJaeOKRjMUUfPG1Sn4HvcuSlXEHKNgzddOflemMW0goa9lWBfaChw1xje7ltEgL3DsX09ZryBZIAEpCR5oj8O9QOfMqkQVOv3PPD9Y9iSCGIrz1jb9RnSvggU9Tw_29_7TdVQYqUZPahLkT1IDq3CXhN79wlr3RMBuH3P3hzsZX7HBGn44FGmsImLNvuy1Q7tSS3Y6g_0QjC6-Ia-JSfwl3iyucCuJJ0bX4BW8K1lxuaTqcr7sWX7UGTdrKwTjGF0TQMzq7K31xbn1nKQunNda50rtxtuUfVlKThi047SVqB7kAQuKYsY-lDdcChEgCiNLAOYyGiXI9IW8KUchV8F1io=w524-h222-no'
+ }}
+          />
+      <Text>         </Text>  //Used for spacing between button and image
+      //Button
+      <Button color='dodgerblue' title= " Go to Home " onPress={() => navigate('Home')} />
       </View>
     )
   }
@@ -95,6 +111,7 @@ class MapScreen extends React.Component {
 
   render() {
     return (
+        //Render a map using provided information (currently centered on Aldrich Park)
         <MapView
           provider= { PROVIDER_GOOGLE }
           style= { styles.map }
@@ -133,6 +150,7 @@ class LandmarkScreen extends React.Component {
   }
 };
 
+//Screen Navigation class
 const RootNavigator = StackNavigator({
   Load: {
     screen: LoadScreen,
@@ -186,8 +204,10 @@ const RootNavigator = StackNavigator({
 { headerMode: 'screen' }
 );
 
+//Used for initial running when app is opened
 export default RootNavigator;
 
+//Style Sheets for other classes (can be referred in style section of each object, such as map for mapstyle)
 const styles = StyleSheet.create({
   map: {
     position: 'absolute',
